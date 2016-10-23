4b098d6fff3acffdbb581ed5e9c2899ac7bde32c
Mahesh Sawant <Mahesh@Maheshs-MacBook-Pro.local>
2016-10-22 20:19:45 -0700

    preparing for changes

--- www/js/selectingword.js
+++ www/js/selectingword.js
@@ -7,11 +7,11 @@ EPUBJS.Hooks.register("beforeChapterDisplay").selectword = function(callback, re
 
     console.log(renderer);
 
-
     var elements = renderer.doc.querySelectorAll('p'),
         items = Array.prototype.slice.call(elements);
 
     items.forEach(function(item){
+
 
         $(item).on('dblclick',function(){
             console.log(item);
@@ -52,7 +52,10 @@ EPUBJS.Hooks.register("beforeChapterDisplay").selectword = function(callback, re
                     //Change data.source to data.something , where something is whichever part of the object you want returned.
                     //To see the whole object you can output it to your browser console using:
                     //console.log(data);
-                    alert(data.word+" : "+data.definitions[0].definition);
+                    console.log(data.word+" : "+data.definitions[0].definition);
+
+
+                        //var amaze = $('<link rel="stylesheet" href = "../www/css/pop.css"><div style="height: 100px;">are you seeing this</div>');
 
                     //document.getElementById("output").innerHTML = data.word;
 
@@ -65,13 +68,64 @@ EPUBJS.Hooks.register("beforeChapterDisplay").selectword = function(callback, re
                 },
                 error: function(err) { alert(err); },
                 beforeSend: function(xhr) {
-                    xhr.setRequestHeader("X-Mashape-Authorization", "CgCJYxK5TImshi7Bm5GslCFSrNkVp1Ii23yjsnNvrCT8Md9veb"); // Enter here your Mashape key
+                    //xhr.setRequestHeader("X-Mashape-Authorization", "CgCJYxK5TImshi7Bm5GslCFSrNkVp1Ii23yjsnNvrCT8Md9veb"); // Enter here your Mashape key
                 }
             });
         });
 
 
     });
+
+
+
+
+    //
+    // var trans = renderer.contents.querySelectorAll('[transclusion]'),
+    //     items = Array.prototype.slice.call(trans);
+    //
+    // items.forEach(function(item){
+    //     var src = item.getAttribute("ref"),
+    //         iframe = document.createElement('iframe'),
+    //         orginal_width = item.getAttribute("width"),
+    //         orginal_height = item.getAttribute("height"),
+    //         parent = item.parentNode,
+    //         width = orginal_width,
+    //         height = orginal_height,
+    //         ratio;
+    //
+    //
+    //     function size() {
+    //         width = orginal_width;
+    //         height = orginal_height;
+    //
+    //         if(width > chapter.colWidth){
+    //             ratio = chapter.colWidth / width;
+    //
+    //             width = chapter.colWidth;
+    //             height = height * ratio;
+    //         }
+    //
+    //         iframe.width = width;
+    //         iframe.height = height;
+    //     }
+    //
+    //
+    //     size();
+    //
+    //     //-- resize event
+    //
+    //
+    //     renderer.listenUntil("renderer:resized", "renderer:chapterUnloaded", size);
+    //
+    //     iframe.src = src;
+    //
+    //     //<iframe width="560" height="315" src="http://www.youtube.com/embed/DUL6MBVKVLI" frameborder="0" allowfullscreen="true"></iframe>
+    //     parent.replaceChild(iframe, item);
+    //
+    //
+    // });
+
+
 
 
     if(callback) callback();
---
+++ www/pop.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Mahesh on 10/20/16.
+ */
+
+
+
