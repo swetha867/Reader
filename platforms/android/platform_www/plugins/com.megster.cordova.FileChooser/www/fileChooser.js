50e75e6e00bdd4a3aed5d006277e1625458d07f8
sohe1l <soheilansari@icloud.com>
2019-09-07 16:50:08 -0700

    Removed platforms

--- platforms/android/platform_www/plugins/com.megster.cordova.FileChooser/www/fileChooser.js
+++
@@ -1,8 +0,0 @@
-cordova.define("com.megster.cordova.FileChooser.FileChooser", function(require, exports, module) {
-module.exports = {
-    open: function (success, failure) {
-        cordova.exec(success, failure, "FileChooser", "open", []);
-    }
-};
-
-});
