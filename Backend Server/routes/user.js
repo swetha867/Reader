ec0b54655655b1bd1dd123ece3c626c8148d74b6
sohe1l <soheilansari@icloud.com>
2019-09-23 12:16:17 -0700

    Update user.js

--- Backend Server/routes/user.js
+++ Backend Server/routes/user.js
@@ -22,8 +22,8 @@ router.post('/', (req,res) => {
     var email = req.body.email
     mysqlConnection.query('INSERT INTO Users (`Student_ID`, `Email_ID`) VALUES (?,?) ', 
     [studentID, email], (req,resp) => {
-        console.log("Data Inserted " + result.insertId);
-        res.end(JSON.stringify({ user_id: result.insertId }));
+        console.log("Data Inserted " + resp.insertId);
+        res.end(JSON.stringify({ user_id: resp.insertId }));
     }
     );
     
