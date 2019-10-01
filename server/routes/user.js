a565627768b40284c226b620e8bbc653a00109d6
dhwanshah21 <dhwanshah2230@gmail.com>
2019-10-01 13:20:14 -0700

    if users exists then it wont make another entry in db

--- server/routes/user.js
+++ server/routes/user.js
@@ -18,11 +18,22 @@ router.get('/', (req,res) => {
 router.post('/', (req,res) => {
     var studentID = req.body.student_id;
     var email = req.body.email;
-    mysqlConnection.query('INSERT INTO users (`student_id`, `email`) VALUES (?,?) ', 
-    [studentID, email], (req,resp) => {
-        console.log("Data Inserted " + resp.insertId);
-        //res.send({ user_id: resp.insertId });
-    }
-    );
+    mysqlConnection.query('Select * from users where student_id = ?', [studentID], (err,rows,fields) => {
+        if (!err && rows.length === 0) {
+            mysqlConnection.query('INSERT INTO users (`student_id`, `email`) VALUES (?,?) ', 
+            [studentID, email], (req,resp) => {
+            console.log("Data Inserted " + resp.insertId);
+            res.send({ user_id: resp.insertId });
+            });
+        }
+        else  if (err) {
+            return `Here is the error storing the user: ${err}`;
+        }
+        else if (rows.length !== 0 && !err) {
+            console.log(`User ID:${rows[0].student_id} already exists in the database `);
+            res.send({user_id: rows[0].student_id});
+        }
+    })
+    
 })
 module.exports = router;
