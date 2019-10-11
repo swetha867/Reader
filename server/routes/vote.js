11e0b79f942a953841a2e95bf883fef1c9192add
dhwanshah21 <dhwanshah2230@gmail.com>
2019-10-10 23:28:38 -0700

    implemented the feature of handling book id in backend and also adding books to the db

--- server/routes/vote.js
+++ server/routes/vote.js
@@ -20,17 +20,35 @@ router.get('/', (req,res) => {
 
 router.post('/', (req,res) => {
     var userID = req.body.user_id;
-    var bookID = req.body.book_id;
+    var bookID;
     var wordID = req.body.word_id;
     var meaningID = req.body.meaning_id;
     var sentence = req.body.sentence;
-    mysqlConnection.query('INSERT INTO votes (`user_id`, `book_id`, `word_id`, `meaning_id`, `sentence`) VALUES (?,?,?,?,?) ',
-    [userID, bookID, wordID, meaningID, sentence], (req,resp) => {
-        console.log("vote details inserted!");
-    }
-    );
+    var book_name = req.body.book_name;
+    var author_name = req.body.author_name;
 
+
+    mysqlConnection.query(`Select id from books where book_name=? and author_name=?;`,[book_name,author_name], (err,result,fields) => {
+        console.log(result.length === 0);
+        console.log(result);
+        if (result.length === 0) {
+            // Storing the book with book name and author name in the Books Table
+            mysqlConnection.query('Insert into books (`book_name`, `author_name`) VALUES (?,?);', [book_name,author_name], (err,result,fields) => {
+            });
+        }
+        // Returning the bookid for the given book name and author name
+        mysqlConnection.query(`Select id from books where book_name=? and author_name=?;`,[book_name,author_name], (err,result,fields) => {
+            bookID = result[0].id;
+            // Storing all the attributes in votes table with book id
+            mysqlConnection.query('INSERT INTO votes (`user_id`, `book_id`, `word_id`, `meaning_id`, `sentence`) VALUES (?,?,?,?,?) ',
+            [userID, bookID, wordID, meaningID, sentence], (req,resp) => {
+            console.log("vote details inserted!");
+            res.send({response: "Vote Details Inserted!"});
+            });
+        });
 });
-
+    // console.log('outside',bookID);
+    
+});
 
 module.exports = router;
