317d93c269981d45cc5c2871a2c2dc074bf67348
sohe1l <soheilansari@icloud.com>
2019-10-21 13:07:47 -0700

    Fix vote with updating votes

--- server/routes/vote.js
+++ server/routes/vote.js
@@ -1,16 +1,16 @@
 
 var express = require('express');
-const mysqlConnection = require('../database/db');
+const db = require('../database/db');
+const book = require('../database/book');
 const router = express.Router();
 
 // For now GET call is to see the data on postman
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     mysqlConnection.query('Select * from votes', (err, rows, fields) => {
-        if(!err) {
+        if (!err) {
             res.send(rows);
             console.log('Information from Votes Table Fetched');
-        }
-        else {
+        } else {
             res.send(`Error fetching the voting details: ${err}`);
         }
     })
@@ -19,67 +19,65 @@ router.get('/', (req,res) => {
 // POST call to enter the information in votes Table
 // Meaning id 0 means student is not sure which meaning is selected
 // When student lookup for word anytime, we should recieve a post request here with meaningId as null
-router.post('/', (req,res) => {
+router.post('/', (req, res) => {
+    handleVote(req).then(results => res.send(results));
+});
+
+
+
+async function handleVote(req) {
+
+    var bookID = await book.getBookId(req.body.book_name, req.body.author_name);
     var userID = req.body.user_id;
-    var bookID;
     var wordID = req.body.word_id;
     var meaningID = req.body.meaning_id;
     var sentence = req.body.sentence;
-    var book_name = req.body.book_name;
-    var author_name = req.body.author_name;
 
-//     bookID = 1; // must fetch from db
-//     if(wordID == ""){
-//         res.send({res: 'error'});
-//         return;
-//     }
-//     mysqlConnection.query('INSERT INTO votes (`user_id`, `book_id`, `word_id`, `meaning_id`, `sentence`) VALUES (?,?,?,?,?) ',
-//     [userID, bookID, wordID, meaningID, sentence], (error, results, fields) => {
-//         if(error){
-//             res.send({res: 'error'});
-//             return;
-//         }
-//         console.log("vote details inserted!");
-//         res.send({res: 'success'});
-//     }
-//     );
-// });
-    if(wordID == ""){
-            res.send({res: 'error'});
-            return;
-    }
+    return new Promise(function (resolve, reject) {
 
-    mysqlConnection.query(`Select id from books where book_name=? and author_name=?;`,[book_name,author_name], (err,result,fields) => {
-        if(err) {
-            res.send({res: 'error'});
+        if (wordID == "") {
+            resolve({ res: 'error' });
             return;
         }
-        if (result.length === 0) {
-            // Storing the book with book name and author name in the Books Table
-            mysqlConnection.query('Insert into books (`book_name`, `author_name`) VALUES (?,?);', [book_name,author_name], (err,result,fields) => {
-                if(err) {
-                    res.send({res: 'error'});
+
+        db.query('SELECT * FROM votes WHERE user_id = ? AND book_id = ? AND word_id = ? AND sentence = ?',
+            [userID, bookID, wordID, sentence], (err, rows, fields) => {
+                if (err) {
+                    console.log(`Here is the error ${err}`)
+                    resolve(`Here is the error ${err}`);
                     return;
                 }
+                if (rows.length == 0) {
+                    // Storing all the attributes in votes table with book id
+                    db.query('INSERT INTO votes (`user_id`, `book_id`, `word_id`, `meaning_id`, `sentence`) VALUES (?,?,?,?,?) ',
+                        [userID, bookID, wordID, meaningID, sentence], (err, rows, fields) => {
+                            if (err) {
+                                console.log(`Here is the error ${err}`)
+                                resolve(`Here is the error ${err}`);
+                                return;
+                            }
+                            console.log("vote details inserted!");
+                            resolve({ response: "Vote Details Inserted!" });
+                        });
+                } else {
+                    // Storing all the attributes in votes table with book id
+                    db.query('UPDATE votes SET meaning_id = ? WHERE user_id = ? AND book_id = ? AND word_id = ? AND sentence = ?',
+                        [meaningID, userID, bookID, wordID, sentence], (err, rows, fields) => {
+                            if (err) {
+                                console.log(`Here is the error ${err}`)
+                                resolve(`Here is the error ${err}`);
+                                return;
+                            }
+                            console.log("vote details updated!");
+                            resolve({ response: "Vote Details updated!" });
+                        });
+                }
             });
-        }
-        // Returning the bookid for the given book name and author name
-        mysqlConnection.query(`Select id from books where book_name=? and author_name=?;`,[book_name,author_name], (err,result,fields) => {
-            if(err) {
-                res.send({res: 'error'});
-                return;
-            }
-            bookID = result[0].id;
-            // Storing all the attributes in votes table with book id
-            mysqlConnection.query('INSERT INTO votes (`user_id`, `book_id`, `word_id`, `meaning_id`, `sentence`) VALUES (?,?,?,?,?) ',
-            [userID, bookID, wordID, meaningID, sentence], (req,resp) => {
-            console.log("vote details inserted!");
-            res.send({response: "Vote Details Inserted!"});
-            });
-        });
-});
-    // console.log('outside',bookID);
-    
-});
+    });
+
+}
+
+
+
 
 module.exports = router;
