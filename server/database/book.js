808b88f06db7c93b8ef31177798b650f766cc35c
sohe1l <soheilansari@icloud.com>
2019-10-21 12:18:13 -0700

    Create book.js

---
+++ server/database/book.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const db = require('../database/db');
+
+var book = {
+    getBookId: async function (title, author) {
+        return new Promise(function (resolve, reject) {
+
+            db.query(`SELECT id FROM books where book_name=? and author_name=?;`, [title, author], (err, result, fields) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                if (result.length === 0) {
+                    // Storing the book with book name and author name in the Books Table
+                    mysqlConnection.query('INSERT into books (`book_name`, `author_name`) VALUES (?,?);', [title, author], (err, result, fields) => {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+                        resolve(result[0].id);
+                    });
+                } else {
+                    resolve(result[0].id);
+                }
+            });
+        });
+    },
+}
+
+
+module.exports = dic;
