a2cd96be05a08917e45adb18fd8634d57298e205
sohe1l <soheilansari@icloud.com>
2019-10-22 16:33:08 -0700

    Add inst-students

--- server/routes/inst.js
+++ server/routes/inst.js
@@ -9,9 +9,26 @@ router.get('/login', (req, res) => {
 })
 
 router.get('/students', (req, res) => {
-  console.log(req.isAuthenticated());
-  console.log(req.user);
-  res.send(req.user);
+  getStudents(req, res);
 })
+
+async function getStudents(req, res){
+  db.query('SELECT * FROM users', [], (err, rows, fields) => {
+    if (err) {
+      res.send(err);
+    }else{
+      res.render('inst/students', { students: rows});
+    }
+  })
+
+}
+
+
+
+// async function getAllUsers(user_id, word_id) {
+//   return new Promise(function (resolve, reject) {
+
+//   });
+// }
 
 module.exports = router;
