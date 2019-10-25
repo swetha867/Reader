4854058683f03ecaa68b6926f8a843e7d78b8e6f
sohe1l <soheilansari@icloud.com>
2019-10-24 17:56:14 -0700

    Update instructor voting

--- server/routes/inst.js
+++ server/routes/inst.js
@@ -29,27 +29,51 @@ router.get('/vote', (req, res) => {
 })
 
 async function getVote(req, res){
-  db.query(`SELECT word, sentence,
-  GROUP_CONCAT(CONCAT('{"id":"', dm.id, '", "fl":"',dm.fl,'", "meaning":"',dm.meaning,'"}')) meanings
-  FROM votes v 
-  JOIN dictionary_meanings dm ON v.word_id = dm.word_id
-  JOIN dictionary_words dw ON dw.id = dm.word_id
-  WHERE v.meaning_id = 0 GROUP BY v.word_id;`, [],
-  (err, rows, fields) => {
-    if (err) {
-      res.send(err);
-    }else{
-      res.render('inst/vote', { votes: rows});
-    }
-  })
+  const books = await getAllBooks();
 
+  for (i = 0; i < books.length; i++) {
+    books[i].votes = await getVotesForBook(books[i].id);
+  }
+  console.log('rendering');
+  res.render('inst/vote', { books: books});
 }
 
 
-// async function getAllUsers(user_id, word_id) {
-//   return new Promise(function (resolve, reject) {
+async function getAllBooks() {
+  return new Promise(function (resolve, reject) {
+    db.query(`SELECT * FROM books ORDER BY book_name`, [],
+    (err, rows, fields) => {
+      if (err) {
+        resolve(null);
+        return;
+      }else{
+        resolve(rows);
+        return;
+      }
+    })
+  });
+}
 
-//   });
-// }
+async function getVotesForBook(book_id) {
+  return new Promise(function (resolve, reject) {
+    db.query(`SELECT v.id vote_id, v.word_id, word, sentence,
+    GROUP_CONCAT(CONCAT('{"id":"', dm.id, '", "fl":"',dm.fl,'", "meaning":"',dm.meaning,'"}')) meanings
+    FROM votes v 
+    JOIN dictionary_meanings dm ON v.word_id = dm.word_id
+    JOIN dictionary_words dw ON dw.id = dm.word_id
+    WHERE v.meaning_id = 0 
+    AND v.book_id = ?
+    GROUP BY v.word_id;`, [book_id],
+    (err, rows, fields) => {
+      if (err) {
+        resolve(null);
+        return;
+      }else{
+        resolve(rows);
+        return;
+      }
+    })
+  });
+}
 
 module.exports = router;
