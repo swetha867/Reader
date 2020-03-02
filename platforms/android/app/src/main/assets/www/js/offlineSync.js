30e548880cf3691ac471f7b34cea3cb94b5de0df
dhwanshah21 <dhwanshah2230@gmail.com>
2020-03-01 18:49:37 -0800

    offline Sync js file added to take the data from local

--- platforms/android/app/src/main/assets/www/js/offlineSync.js
+++ platforms/android/app/src/main/assets/www/js/offlineSync.js
@@ -1,12 +1,24 @@
 
-var db = window.sqlitePlugin.openDatabase({ name: 'demo.db', location: 'default' });
+
 
 function offlineSync() {
+    var db = window.sqlitePlugin.openDatabase({ name: 'demo.db', location: 'default' });
     db.transaction(function (tx) {
         tx.executeSql("Select * from PageTable", [], function querySuccess (tx,results) {
-            console.log("Results are:", results);
+            console.log('temp', results.rows.item(1));
+
+            if (results.rows.length === 0) {
+                alert('Already Synced with Cloud DB!');
+            }
+            else {
+                var syncResults = [];
+                for (var i=0; i < results.rows.length; i++) {
+                syncResults.push(results.rows.item(i));
+                }
+                console.log('SyncResults:',syncResults);                 
+            }
         }, function error (tx, err) {
             console.log("Error is", err);
-        })
+        });
     })
 }
